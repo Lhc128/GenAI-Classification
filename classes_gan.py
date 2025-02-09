c4f3724fc58a3247bbefb60766a8d642934f60fc
Lhc128 <lucashcc8@gmail.com>
2025-02-09 10:47:07 -0300

    faca as mudancas necessarias

---
+++ classes_gan.py
@@ -0,0 +1,7 @@
+from tensorflow.keras.models import Sequential
+#from tensorflow.keras.layers import Conv2D, Dense, Flatten, Reshape, LeakyReLU, Droupout, UpSampling
+
+class Discriminator():
+    def __init__():
+        return
+    
